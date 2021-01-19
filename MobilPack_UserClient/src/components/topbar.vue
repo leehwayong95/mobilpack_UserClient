0849abb67bfce690e165e6bc7946a53a19832043
DESKTOP-QIDRDD2\82107 <rmvmf123@naver.com>
2021-01-19 16:42:08 +0900

    유저 스프링 초기 빌드 완료

--- MobilPack_UserClient/src/components/topbar.vue
+++ MobilPack_UserClient/src/components/topbar.vue
@@ -2,11 +2,11 @@
   <div class="srch_box">
     <h1 v-on:click="jump('recommands')"> 관광지 추천 <br>&nbsp;&nbsp;&nbsp;페이지</h1>
     <div align="right">
-    <button class="btn" v-on:click="jump('recommands')">추천장소</button>
-    <button class="btn"  v-on:click="jump('notice')">공지사항</button>
-    <button class="btn"  v-on:click="jump('qna')">문의사항</button>
-    <button class="btn" v-on:click="jump('myinfo')">사용자 이름</button>
-    <button class="btn" @click="Logout">로그아웃</button>
+        <button class="btn" v-on:click="jump('recommands')">추천장소</button>
+        <button class="btn"  v-on:click="jump('notice')">공지사항</button>
+        <button class="btn"  v-on:click="jump('qna')">문의사항</button>
+        <button class="btn" v-on:click="jump('myinfo')">사용자 이름</button>
+        <button class="btn" @click="Logout">로그아웃</button>
     </div>
   </div >
 </template>
@@ -29,6 +29,9 @@ export default {
     font-size: 30px;
     color: aliceblue;
 }
+.srch_box>*{
+    display: inline-block;
+}
 .btn {
     /* margin이 버튼 간격 입니다 */
     margin-right: 60px;
@@ -37,10 +40,14 @@ export default {
     color: aliceblue;
 }
 .srch_box {
+     /* flex, justify-content, align-items:공부 필요합니다 */
     position: relative;
     width: 100%;
     padding: 20px;
     background: #2c3a52;
     border-radius: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
