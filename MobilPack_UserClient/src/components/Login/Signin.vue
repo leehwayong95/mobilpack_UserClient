c5f5d3e3d0664587247f03a864d8b3980b6992f9
Lee Hwayong <dlghkdyd500@gmail.com>
2021-01-21 11:10:36 +0900

    add method for user Signin page

--- MobilPack_UserClient/src/components/Login/Signin.vue
+++ MobilPack_UserClient/src/components/Login/Signin.vue
@@ -6,27 +6,27 @@
       </div>
       <table>
         <tr><td>
-          <input type="text" class="login_txt" placeholder="ID" v-model="id"/>
+          <input type="text" id='signin_id' class="login_txt" placeholder="ID" v-model="id"/>
         </td></tr>
         <tr><td>
-          <button type="button" class="login_btn">중복확인</button>
+          <button type="button" id='signin_check' class="login_btn" @click='checkID'>중복확인</button>
         </td></tr>
         <tr><td>
           <input
           type="password" class="login_txt" placeholder="비밀번호"
-          v-model="pw"/>
+          v-model="pw" id='signin_pw'/>
         </td></tr>
         <tr><td>
           <input
           type="password" class="login_txt" placeholder="비밀번호 확인"
-          v-model="confirmpw"/>
+          v-model="confirmpw" id='signin_confirm'/>
         </td></tr>
       </table>
       <table>
-        <tr><td><input type="text" placeholder="이름" v-model="name"/></td></tr>
-        <tr><td><input type="text" placeholder="전화번호" v-model="phone"/></td></tr>
+        <tr><td><input type="text" placeholder="이름" v-model="name" id='signin_name'/></td></tr>
+        <tr><td><input type="text" placeholder="전화번호" v-model="phone" id='signin_phone'/></td></tr>
         <tr><td>
-          <select name="contry" v-model="contry">
+          <select id="signin_contry" name="contry" v-model="contry">
           <option value="">국가 선택</option>
           <option value="KR">대한민국</option>
           <option value="US">미국</option>
@@ -36,9 +36,9 @@
         </td></tr>
       </table>
       <table>
-        <tr><td><input type="checkbox" v-model="allagree"><span>전체동의</span></td></tr>
-        <tr><td><input type="checkbox" v-model="serviceagree"><span>(필수) 서비스 이용약관</span></td></tr>
-        <tr><td><input type="checkbox" v-model="infomationagree"><span>(필수) 개인정보 처리 방침</span></td></tr>
+        <tr><td><input type="checkbox" v-model="allagree" v-on:click="agree" ><span>전체동의</span></td></tr>
+        <tr id='service_agree'><td><input type="checkbox" v-model="serviceagree"><span>(필수) 서비스 이용약관</span></td></tr>
+        <tr id='info_agree'><td><input type="checkbox" v-model="infomationagree"><span>(필수) 개인정보 처리 방침</span></td></tr>
       </table>
       <div class = 'btn_wrap'>
         <button type="button" class="login_btn" @click="signin">회원가입</button>
@@ -49,22 +49,100 @@
 
 <script>
 export default {
+  updated () {
+    this.allagree = this.serviceagree && this.infomationagree
+  },
   data () {
     return {
       id: '',
+      check: false,
       pw: '',
       confirmpw: '',
       name: '',
       phone: '',
       contry: '',
-      allagree: (this.serviceagree && this.infomationagree),
+      allagree: false,
       serviceagree: false,
       infomationagree: false
     }
   },
   methods: {
     signin () {
-      alert(this.allagree)
+      let check = {
+        signin_id: this.id,
+        signin_check: this.check,
+        signin_pw: this.pw,
+        signin_confirm: this.confirmpw,
+        signin_name: this.name,
+        signin_phone: this.phone,
+        signin_contry: this.contry,
+        service_agree: this.serviceagree,
+        info_agree: this.infomationagree
+      }
+      let message = {
+        signin_id: '아이디를 입력해주세요',
+        signin_check: '아이디 중복체크를 해주세요',
+        signin_pw: '비밀번호를 입력해주세요',
+        signin_confirm: '확인비밀번호가 다릅니다',
+        signin_name: '이름을 입력해주세요',
+        signin_phone: '전화번호를 입력해주세요',
+        signin_contry: '거주나라를 선택해주세요',
+        service_agree: '서비스 이용 약관에 동의해주세요',
+        info_agree: '개인 정보 처리방침에 동의해주세요'
+      }
+      let checkingItem = null
+      for (let i in check) {
+        if (check[i] === '' || check[i] === false) {
+          alert(message[i])
+          document.getElementById(i).focus()
+          checkingItem = i
+          break
+        }
+      }
+      if (checkingItem === null) {
+        let signinItem = {
+          user_id: this.id,
+          password: this.pw,
+          name: this.name,
+          phone: this.phone,
+          contry: this.contry
+        }
+        this.$axios.post('http://localhost:9000/api/user/signin', signinItem)
+          .then((res) => {
+            if (res.data.status) {
+              alert('회원가입이 정상적으로 처리되었습니다.')
+              this.$router.push('/')
+            } else {
+              alert('회원가입 실패')
+            }
+          })
+          .catch((err) => {
+            alert(err.message)
+          })
+      }
+    },
+    checkID () {
+      if (this.id === '') {
+        alert('아이디를 입력해주세요')
+        document.getElementById('signin_id').focus()
+      } else {
+        this.$axios.post('http://localhost:9000/api/user/check', {
+          id: this.id
+        })
+          .then((res) => {
+            if (res.data.status) {
+              alert('사용할 수 있는 아이디입니다.')
+              document.getElementById('signin_id').disabled = true
+              this.check = true
+            } else {
+              alert('사용할 수 없는 아이디입니다.')
+            }
+          })
+      }
+    },
+    agree () {
+      this.serviceagree = !this.allagree
+      this.infomationagree = !this.allagree
     }
   }
 }
