64b2174251f015f37bcce58c1f96b6e3911aaf7d
Lee Hwayong <dlghkdyd500@gmail.com>
2021-01-21 15:37:41 +0900

    add regular expression for signin Component

--- MobilPack_UserClient/src/components/Login/Signin.vue
+++ MobilPack_UserClient/src/components/Login/Signin.vue
@@ -6,7 +6,7 @@
       </div>
       <table>
         <tr><td>
-          <input type="text" id='signin_id' class="login_txt" placeholder="ID" v-model="id"/>
+          <input type="text" id='signin_id' class="login_txt" placeholder="이메일" v-model="id"/>
         </td></tr>
         <tr><td>
           <button type="button" id='signin_check' class="login_btn" @click='checkID'>중복확인</button>
@@ -36,9 +36,9 @@
         </td></tr>
       </table>
       <table>
-        <tr><td><input type="checkbox" v-model="allagree" v-on:click="agree" ><span>전체동의</span></td></tr>
-        <tr id='service_agree'><td><input type="checkbox" v-model="serviceagree"><span>(필수) 서비스 이용약관</span></td></tr>
-        <tr id='info_agree'><td><input type="checkbox" v-model="infomationagree"><span>(필수) 개인정보 처리 방침</span></td></tr>
+        <tr>전체동의<td><span><input type="checkbox" v-model="allagree" v-on:click="agree" ></span></td></tr>
+        <tr id='service_agree'><span>(필수) 서비스 이용약관</span><td><input type="checkbox" v-model="serviceagree"></td></tr>
+        <tr id='info_agree'><span>(필수) 개인정보 처리 방침</span><td><input type="checkbox" v-model="infomationagree"></td></tr>
       </table>
       <div class = 'btn_wrap'>
         <button type="button" class="login_btn" @click="signin">회원가입</button>
@@ -66,8 +66,26 @@ export default {
       infomationagree: false
     }
   },
+  watch: {
+    id () {
+      if (!this.check) {
+        this.id = this.id.replace(/[^A-Za-z0-9@._+]/g, '')
+      }
+    },
+    pw () {
+      this.pw = this.pw.replace(/[^A-Za-z0-9+]/g, '')
+    },
+    name () {
+      this.name = this.name.replace(/[^ㄱ-ㅎ가-힣+]/g, '')
+    },
+    phone () {
+      this.phone = this.phone.replace(/[^0-9+]{12}/g, '')
+    }
+  },
   methods: {
+    // 회원가입 요청 메서드
     signin () {
+      // 회원가입시 체크하는 리스트
       let check = {
         signin_id: this.id,
         signin_check: this.check,
@@ -79,6 +97,7 @@ export default {
         service_agree: this.serviceagree,
         info_agree: this.infomationagree
       }
+      // 부적격시 안내 메시지 리스트
       let message = {
         signin_id: '아이디를 입력해주세요',
         signin_check: '아이디 중복체크를 해주세요',
@@ -90,23 +109,30 @@ export default {
         service_agree: '서비스 이용 약관에 동의해주세요',
         info_agree: '개인 정보 처리방침에 동의해주세요'
       }
-      let checkingItem = null
+      // 리스트 점검 flag
+      let checkingItem = true
+      // 리스트 점검 반복문
       for (let i in check) {
         if (check[i] === '' || check[i] === false) {
           alert(message[i])
-          document.getElementById(i).focus()
-          checkingItem = i
+          document.getElementById(i).focus() // 부적격 아이템 focus
+          checkingItem = false// 부적격 아이템 발견시 flag 초기화
           break
         }
       }
-      if (checkingItem === null) {
-        let signinItem = {
+      if (checkingItem && !this.phoneCheck(this.phone)) {
+        alert('휴대폰 번호가 올바르지 않습니다.\n다시 입력 해주세요')
+        checkingItem = false
+      }
+      if (checkingItem) { // flag가 초기값 그대로일 때
+        let signinItem = { // 필요 아이템 Json형식에 맞춰 담기
           user_id: this.id,
           password: this.pw,
           name: this.name,
           phone: this.phone,
           contry: this.contry
         }
+        // 회원가입 요청
         this.$axios.post('http://localhost:9000/api/user/signin', signinItem)
           .then((res) => {
             if (res.data.status) {
@@ -121,28 +147,54 @@ export default {
           })
       }
     },
+    // 아이디 중복 확인 메서드
     checkID () {
       if (this.id === '') {
         alert('아이디를 입력해주세요')
         document.getElementById('signin_id').focus()
+        // 공백입력시 아이디 입력칸 focus
       } else {
-        this.$axios.post('http://localhost:9000/api/user/check', {
-          id: this.id
-        })
-          .then((res) => {
-            if (res.data.status) {
-              alert('사용할 수 있는 아이디입니다.')
-              document.getElementById('signin_id').disabled = true
-              this.check = true
-            } else {
-              alert('사용할 수 없는 아이디입니다.')
-            }
+        if (this.emailCheck(this.id)) {
+          // 중복확인 요청
+          this.$axios.post('http://localhost:9000/api/user/check', {
+            id: this.id
           })
+            .then((res) => {
+              if (res.data.status) {
+                this.check = true
+                // 중복 확인 flag 활성화
+                alert('사용할 수 있는 아이디입니다.')
+                document.getElementById('signin_id').disabled = true
+                // 사용할 수 있는 아이디이면 변경 불가하게 disabled
+              } else {
+                alert('사용할 수 없는 아이디입니다.')
+              }
+            })
+            .catch((err) => {
+              console.log(err.message)
+              alert('서버 오류입니다.\n개발자가 열심히 일하고있습니다.\n잠시 후 시도해주세요')
+            })
+        } else {
+          alert('이메일 형식이 아닙니다. 다시 입력 해주세요')
+          document.getElementById('signin_id').focus()
+        }
       }
     },
+    // 전체 동의 메서드
     agree () {
       this.serviceagree = !this.allagree
       this.infomationagree = !this.allagree
+    },
+    // 정규식 검사 메서드 //
+    emailCheck (asEmail) {
+      var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+      return regExp.test(asEmail)
+    },
+    phoneCheck (asPhone) {
+      var regExp = /^01(?:0|1|[6-9])(?:\d{3}|\d{4})\d{4}$/
+      return regExp.test(asPhone)
+    },
+    pwCheck () {
     }
   }
 }
