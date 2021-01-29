ea42118be7b9fb64cc2a6eb9212f1a487788775b
Lee Hwayong <dlghkdyd500@gmail.com>
2021-01-29 14:47:35 +0900

    edit table Header List for Qna

--- MobilPack_UserClient/src/components/Qna/QnaList.vue
+++ MobilPack_UserClient/src/components/Qna/QnaList.vue
@@ -53,11 +53,7 @@
           <col width="20%">
         </colgroup>
         <tr>
-          <th rowspan="2">No.</th>
-          <th colspan="3">문의 정보</th>
-          <th colspan="3">답변 정보</th>
-        </tr>
-        <tr>
+          <th>No.</th>
           <th scope="col">문의 유형</th>
           <th scope="col">문의 제목</th>
           <th scope="col">문의 일시</th>
