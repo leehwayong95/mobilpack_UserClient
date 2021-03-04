21debc4d161ab4e9378c17df8d987e974bb00fa5
DESKTOP-QIDRDD2\82107 <rmvmf123@naver.com>
2021-03-04 16:56:34 +0900

    공지사항 상세 내용 왼쪽 정렬

--- MobilPack_UserClient/src/components/Notice/Details.vue
+++ MobilPack_UserClient/src/components/Notice/Details.vue
@@ -10,7 +10,7 @@
         <col style="width: 100px;">
         <col style="width: 100px;">
         <col style="width: 150px;">
-        <col style="width: 120px;">
+        <col style="width: 100px;">
         <col style="width: 50px;">
         <col style="width: 200px;">
         </colgroup>
@@ -67,12 +67,15 @@ export default
     this.$axios.get('http://localhost:9000/api/detail', {params: {postindex: this.$route.query.index}})
       .then((res) => {
         this.items = res.data
+        this.items.createat = this.items.createat.substring(0, 16)
+        if (this.items.updateat) {
+          this.items.updateat = this.items.updateat.substring(0, 16)
+        }
         this.postindex = this.$route.query.index
         this.hypercontent = res.data.content
         this.hypercontent = this.test(this.hypercontent)
         this.testcontent = res.data.content
         this.result = this.testcontent.match(this.hyperlink)
-        console.log(this.items)
       })
       .catch((err) => {
         console.log(err)
@@ -112,7 +115,7 @@ export default
   width: 100px;
   height: 30px;
 }
-#content table td {
+#center table td {
     text-align: left;/* items 내용들 왼쪽 정렬 */
 }
 #content > .cont_inner {
